92cc03ef5f1dfc41087a2f8777e144f541be1192
Steven Rick <srick@ucsd.edu>
2013-10-23 15:24:08 -0700

    Pushing refactored files to branch

---
+++ src/com/chjns/cpraid/HelpScreenTwo.java
@@ -0,0 +1,62 @@
+package com.chjns.cpraid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by NarineC on 10/22/13.
+ */
+public class HelpScreenTwo extends Activity {
+
+    Intent switchToMainActivity;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.help_screen_two);
+        switchToMainActivity = new Intent(this, MainActivity.class);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            // Handle tap events.
+            case KeyEvent.KEYCODE_DPAD_CENTER:
+                //  startActivity(new Intent(this, MainActivity.class));
+                //  count = 5000;
+            case KeyEvent.KEYCODE_ENTER:
+                //count = 0000;
+                //  setContentView(R.layout.activity_main);
+                //  toggleStopWatch();
+                return true;
+            default:
+                startActivity(switchToMainActivity);
+                //setContentView(R.layout.activity_main);
+                return super.onKeyDown(keyCode, event);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+    }
+
+}
